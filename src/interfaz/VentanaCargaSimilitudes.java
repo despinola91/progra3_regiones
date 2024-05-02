5794e483a22269ee4dc3566ebac67f9dc6efec04
AngelGabrielAcunia <angelgabriel.acunia@gmail.com>
2024-05-01 21:23:11 -0300

    Update VentanaCargaSimilitudes.java

--- src/interfaz/VentanaCargaSimilitudes.java
+++ src/interfaz/VentanaCargaSimilitudes.java
@@ -7,7 +7,7 @@ public class VentanaCargaSimilitudes extends JFrame {
     public VentanaCargaSimilitudes() {
         setTitle("Cargar Similitudes");
         setResizable(false);
-        setBounds(300, 200, 300, 200);
+        setBounds(300, 200, 322, 369);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 }
