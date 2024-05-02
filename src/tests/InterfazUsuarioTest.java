ba58b72fe7b21cbf7b27038385f20938a16a3cbf
AngelGabrielAcunia <angelgabriel.acunia@gmail.com>
2024-05-01 22:01:23 -0300

    Create InterfazUsuarioTest.java

---
+++ src/tests/InterfazUsuarioTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+import interfaz.MainForm;
+
+public class InterfazUsuarioTest {
+
+	@Test
+	void dividirRegionesTest() {
+
+	}
+}
